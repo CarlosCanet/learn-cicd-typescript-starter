32a555f69b7a3badef374f7e1be4d3c8c2e3ada7
Carlos Canet <carlos@carlosca.net>
2026-03-05 01:05:01 +0100

    feat: add test and temporary break the getApiKey function

---
+++ src/tests/auth.test.ts
@@ -0,0 +1,31 @@
+import { IncomingHttpHeaders } from "http";
+import { getAPIKey } from "../api/auth";
+import { describe, expect, test } from "vitest";
+
+describe("person", () => {
+  test("request without authorization header", () => {
+    const headers: IncomingHttpHeaders = { "origin": "192.168.1.1" };
+    const result = getAPIKey(headers);
+    expect(result).toBeDefined();
+    expect(result).toBeNull();
+  });
+
+  test("request with wrong authorization header", () => {
+    const headers: IncomingHttpHeaders = { "authorization": "ApiKey" };
+    const result = getAPIKey(headers);
+    expect(result).toBeDefined();
+    expect(result).toBeNull();
+    
+    const headers2: IncomingHttpHeaders = { "authorization": "Apikey" };
+    const result2 = getAPIKey(headers2);
+    expect(result2).toBeDefined();
+    expect(result2).toBeNull();
+  });
+
+  test("request with authorization header", () => {
+    const headers: IncomingHttpHeaders = { "authorization": "ApiKey my-api-key" };
+    const result = getAPIKey(headers);
+    expect(result).toBeDefined();
+    expect(result).toBe("my-api-key");
+  });
+});
