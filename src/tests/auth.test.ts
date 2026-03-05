1f6f9fa8217c11390d3292f9d1a6e55611217f31
Carlos Canet <carlos@carlosca.net>
2026-03-05 01:30:22 +0100

    ci: add format job

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -4,26 +4,26 @@ import { describe, expect, test } from "vitest";
 
 describe("person", () => {
   test("request without authorization header", () => {
-    const headers: IncomingHttpHeaders = { "origin": "192.168.1.1" };
+    const headers: IncomingHttpHeaders = { origin: "192.168.1.1" };
     const result = getAPIKey(headers);
     expect(result).toBeDefined();
     expect(result).toBeNull();
   });
 
   test("request with wrong authorization header", () => {
-    const headers: IncomingHttpHeaders = { "authorization": "ApiKey" };
+    const headers: IncomingHttpHeaders = { authorization: "ApiKey" };
     const result = getAPIKey(headers);
     expect(result).toBeDefined();
     expect(result).toBeNull();
-    
-    const headers2: IncomingHttpHeaders = { "authorization": "Apikey" };
+
+    const headers2: IncomingHttpHeaders = { authorization: "Apikey" };
     const result2 = getAPIKey(headers2);
     expect(result2).toBeDefined();
     expect(result2).toBeNull();
   });
 
   test("request with authorization header", () => {
-    const headers: IncomingHttpHeaders = { "authorization": "ApiKey my-api-key" };
+    const headers: IncomingHttpHeaders = { authorization: "ApiKey my-api-key" };
     const result = getAPIKey(headers);
     expect(result).toBeDefined();
     expect(result).toBe("my-api-key");
